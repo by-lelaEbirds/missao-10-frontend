57528af491518b5ec206b5fef0529cad396002a3
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-11-06 09:02:22 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Missão 10 - Frontend (Em breve)</h1>
+      <p>Este é o ponto de partida. Em breve, aqui teremos nosso formulário e nossa lista!</p>
+    </>
+  )
+}
+
+export default App

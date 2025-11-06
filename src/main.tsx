8180c75271e67d7be0a12e698e0fc0443062d8f7
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-11-06 10:26:54 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom' // <-- Importamos o roteador
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* <-- "Ligamos" o roteador em volta do app */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
